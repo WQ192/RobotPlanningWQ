a1ec6f6376eff23343c0a64d3d7c5be0e0d75198
WQ192 <william585811@outlook.com>
2024-11-14 12:58:38 +0000

    Initial Commit

---
+++ RobotPlanningWQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RobotPlanningWQ
+//
+//  Created by William Qiu on 14/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RobotPlanningWQ/RobotPlanningWQApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotPlanningWQApp.swift
+//  RobotPlanningWQ
+//
+//  Created by William Qiu on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RobotPlanningWQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
